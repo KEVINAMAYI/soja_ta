6e1ce346934bfb7ef3c3fb7b3c1696f8f64d358b
kevin-amayi <kevinamayi20@gmail.com>
2026-01-27 10:19:40 +0300

    bug fixes on ta

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             {{-- Always use a full URL. config('app.url') pulls from your .env --}}
-            <img src="{{ config('app.url') }}/images/logos/soja_ta_logo.png" class="logo" alt="{{ config('app.name') }}" style="width: 200px;">
+            <img src="{{ config('app.url') }}/images/logos/soja_ta_logo.png" class="logo" alt="{{ config('app.name') }}" style="width: 230px;">
         </a>
     </td>
 </tr>
