3370a63c6b7757a82448026be255f0b0ba40747e
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-06 17:09:23 +0300

    feat: create LevelApprover model for managing approval levels in leave requests

---
+++ app/Models/LevelApprover.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LevelApprover extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'leave_approval_log_id',
+        'level_approver_id'
+    ];
+
+    public function levelApprover()
+    {
+        return $this->belongsTo(User::class, 'level_approver_id');
+    }
+
+    public static function getActionedApproversCountForLog(int $leave_approval_log_id)
+    {
+        return LevelApprover::where('leave_approval_log_id', $leave_approval_log_id)->count();
+    }
+}
