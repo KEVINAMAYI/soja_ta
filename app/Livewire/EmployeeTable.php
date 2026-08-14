41ac23a291bad48ac4a09b18e02c4eea70a813b6
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-14 11:01:23 +0300

    feat: filter employees by organization and exclude users in EmployeeTable

--- app/Livewire/EmployeeTable.php
+++ app/Livewire/EmployeeTable.php
@@ -124,7 +124,8 @@ class EmployeeTable extends DataTableComponent
 
         $query = Employee::query()
             ->select('employees.*')
-            ->where('organization_id', $orgId);
+            ->where('organization_id', $orgId)
+            ->where('is_user', false);
 
         // For school orgs, eager load the last attendance record per pembroke
         if ($isStudentOrg) {
