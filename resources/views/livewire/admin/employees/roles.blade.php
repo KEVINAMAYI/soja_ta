873f05633e761b167b864289e2ffefed294e1206
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-18 17:06:36 +0300

    feat: update role display to show job title instead of user roles

--- resources/views/livewire/admin/employees/roles.blade.php
+++ resources/views/livewire/admin/employees/roles.blade.php
@@ -1,5 +1,3 @@
-@if ($employee->user)
-    @foreach ($employee->user->getRoleNames() as $role)
-        <span style="background:var(--primary-color) !important; color:white;" class="badge me-2 mb-1">{{ $role }}</span>
-    @endforeach
+@if ($employee->jobTitle())
+    <span style="background:var(--primary-color) !important; color:white;" class="badge me-2 mb-1">{{ $employee->jobTitle()->first()->name }}</span>
 @endif
