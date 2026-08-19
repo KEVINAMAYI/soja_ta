4f9edbc0ea55e1e6332ce31aee8a73a375a89f68
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-19 10:10:14 +0300

    feat: update job title display logic to use null-safe operator

--- resources/views/livewire/admin/employees/roles.blade.php
+++ resources/views/livewire/admin/employees/roles.blade.php
@@ -1,3 +1,3 @@
 @if ($employee->jobTitle())
-    <span style="background:var(--primary-color) !important; color:white;" class="badge me-2 mb-1">{{ $employee->jobTitle()->first()->name }}</span>
+    <span style="background:var(--primary-color) !important; color:white;" class="badge me-2 mb-1">{{ $employee->jobTitle()?->first()?->name }}</span>
 @endif
