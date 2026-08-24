99fb87684f3f0142591be7f9d88a061c85590d88
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-24 10:34:35 +0300

    feat: update logging for mail notifications to use warning level and remove unnecessary info logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,12 +35,6 @@ class AppServiceProvider extends ServiceProvider
                 return;
             }
 
-            Log::info('Mail notification sent', [
-                'notification' => get_class($event->notification),
-                'notifiable_type' => get_class($event->notifiable),
-                'notifiable_id' => $event->notifiable->id ?? null,
-                'response' => $event->response,
-            ]);
         });
 
         Event::listen(NotificationFailed::class, function (NotificationFailed $event): void {
@@ -59,7 +53,7 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(MessageSent::class, function (MessageSent $event): void {
             $message = $event->message;
 
-            Log::info('Mail message sent', [
+            Log::warn('Mail message sent', [
                 'subject' => $message->getSubject(),
                 'to' => array_keys($message->getTo() ?? []),
             ]);
