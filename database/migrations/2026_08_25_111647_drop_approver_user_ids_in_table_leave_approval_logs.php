9bb8d0f1e60008b4cfd38b39024a1b0e69191ea0
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-25 11:39:37 +0300

    fix: remove approver_user_ids column from leave_approval_logs migration

---
+++ database/migrations/2026_08_25_111647_drop_approver_user_ids_in_table_leave_approval_logs.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leave_approval_logs', function (Blueprint $table) {
+            if (Schema::hasColumn('leave_approval_logs', 'approver_user_ids')) {
+                $table->dropColumn('approver_user_ids');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table_leave_approval_logs', function (Blueprint $table) {
+
+            //
+        });
+    }
+};
