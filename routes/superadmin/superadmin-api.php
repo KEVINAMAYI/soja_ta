91157bec0e6666d0691ef1385a1b5c71f7cb69b8
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-26 16:44:08 +0300

    feat: add routes for filtering user activity and audit logs in superadmin API

--- routes/superadmin/superadmin-api.php
+++ routes/superadmin/superadmin-api.php
@@ -1,8 +1,19 @@
 <?php
 
 use App\Http\Controllers\SuperAdmin\Auth\SuperAdminAuth;
+use App\Http\Controllers\SuperAdmin\Logs\LogController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 Route::prefix('super-man')->group(function () {
     Route::post('/login', [SuperAdminAuth::class, 'login']);
 });
+
+Route::prefix('super-man')->middleware([
+    'auth:sanctum',
+    RoleMiddleware::class . ':super-admin',
+])->group(function () {
+    Route::get('/user-activity-logs/filter', [LogController::class, 'filterUserActivityLogs']);
+    Route::get('/audit-logs/filter', [LogController::class, 'filterAuditLogs']);
+});
+
