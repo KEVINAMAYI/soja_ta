a1a78cfd9e314f9db7b65b17a1e987d9a5d9112e
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-26 14:33:13 +0300

    feat: add ApiConstants class for standardized API response codes

---
+++ app/Utils/ApiConstants.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Utils;
+
+class ApiConstants
+{
+    public const SUCCESS_CODE = 1000;
+    public const UNAUTHORIZED_CODE = 1003;
+    public const FORBIDDEN_CODE = 1005;
+}
