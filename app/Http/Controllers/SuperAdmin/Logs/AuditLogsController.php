272f065f398f80185798cfba1cba6475a059d2a9
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-26 15:45:00 +0300

    feat: create AuditLogsController for managing super admin audit logs

---
+++ app/Http/Controllers/SuperAdmin/Logs/AuditLogsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\SuperAdmin\Logs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuditLogsController extends Controller
+{
+    //
+}
