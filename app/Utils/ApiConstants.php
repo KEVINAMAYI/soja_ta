d1daf0c72b6a033879ca22e3a1736e463c588df8
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-26 15:45:29 +0300

    feat: add user action types and system action types constants in ApiConstants

--- app/Utils/ApiConstants.php
+++ app/Utils/ApiConstants.php
@@ -7,4 +7,17 @@ class ApiConstants
     public const SUCCESS_CODE = 1000;
     public const UNAUTHORIZED_CODE = 1003;
     public const FORBIDDEN_CODE = 1005;
+
+
+    // User types
+    public const USER_TYPE_USER = 'USER';
+    public const USER_TYPE_SYSTEM = 'SYSTEM';
+
+    // USER ACTION TYPES
+    public const USER_ACTION_LOGIN = 'LOGIN';
+    public const USER_ACTION_LOGOUT = 'LOGOUT';
+    public const USER_MALICIOUS_ACTIVITY = 'MALICIOUS_ACTIVITY';
+
+    // System action types
+    public const SYSTEM_AUTO_REPORT_GENERATION = 'AUTO_REPORT_GENERATION';
 }
