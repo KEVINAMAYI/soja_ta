64897b73f439b50b8a8615d4462fd9023cf9b883
Sir-Dommy <dominickyengo2017@gmail.com>
2026-08-31 09:57:36 +0300

    feat: add Group attribute to AuditLogsController for improved organization

--- app/Http/Controllers/SuperAdmin/Logs/AuditLogsController.php
+++ app/Http/Controllers/SuperAdmin/Logs/AuditLogsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\SuperAdmin\Logs;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Dedoc\Scramble\Attributes\Group;
 
+#[Group('Superadmin/Logs')]
 class AuditLogsController extends Controller
 {
     //
