e5c79870290660b5c5080d0d305f373cd133e49f
Sir-Dommy <dominickyengo2017@gmail.com>
2026-09-01 08:50:13 +0300

    feat: add user action constants for impersonation management

--- app/Utils/ApiConstants.php
+++ app/Utils/ApiConstants.php
@@ -6,6 +6,7 @@ class ApiConstants
 {
     public const SUCCESS_CODE = 1000;
     public const UNAUTHORIZED_CODE = 1003;
+    public const NOT_FOUND_CODE = 1004;
     public const FORBIDDEN_CODE = 1005;
 
 
@@ -17,6 +18,9 @@ class ApiConstants
     public const USER_ACTION_LOGIN = 'LOGIN';
     public const USER_ACTION_LOGOUT = 'LOGOUT';
     public const USER_MALICIOUS_ACTIVITY = 'MALICIOUS_ACTIVITY';
+    public const USER_ACTION_IMPERSONATION_REQUESTED = 'IMPERSONATION_REQUESTED';
+    public const USER_ACTION_IMPERSONATION_START = 'IMPERSONATION_START';
+    public const USER_ACTION_IMPERSONATION_END = 'IMPERSONATION_END';
 
     // System action types
     public const SYSTEM_AUTO_REPORT_GENERATION = 'AUTO_REPORT_GENERATION';
