94ef69ea6ca2ed461639b9a1fdc6267c7ef0bd21
Sir-Dommy <dominickyengo2017@gmail.com>
2026-09-02 08:21:14 +0300

    fix: ensure return date is correctly formatted using Carbon parsing

--- app/Http/Controllers/APIs/LeaveController.php
+++ app/Http/Controllers/APIs/LeaveController.php
@@ -90,7 +90,7 @@ class LeaveController extends Controller
             if ($employee->shift) {
                 $returnDate = $leaveType->calculateReturnWithEndDate(Carbon::parse($endDate), $employee->shift);
             }
-            $returnDate = $returnDate->format('Y-m-d');
+            $returnDate = Carbon::parse($returnDate)->format('Y-m-d');
             $endDate = Carbon::parse($endDate)->format('Y-m-d');
             
             // Check for conflicts
