5f7755935d8c9d02d0a4238604824e5d75fec981
Sir-Dommy <dominickyengo2017@gmail.com>
2026-09-09 12:07:26 +0300

    feat: create ForgotPasswordRequest for email validation

---
+++ app/Http/Requests/SuperAdmin/Auth/ForgotPasswordRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\SuperAdmin\Auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ForgotPasswordRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'max:255'],
+        ];
+    }
+}
