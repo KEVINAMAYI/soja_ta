7d9ac888f89a093047de15144398e59eba10f59e
Sir-Dommy <dominickyengo2017@gmail.com>
2026-09-09 18:19:23 +0300

    feat: update StoreFeatureRequest to make feature_category_id nullable

--- app/Http/Requests/SuperAdmin/Subscriptions/StoreFeatureRequest.php
+++ app/Http/Requests/SuperAdmin/Subscriptions/StoreFeatureRequest.php
@@ -9,7 +9,7 @@ class StoreFeatureRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'feature_category_id' => ['sometimes', 'integer', 'exists:feature_categories,id'],
+            'feature_category_id' => ['nullable', 'sometimes', 'integer', 'exists:feature_categories,id'],
             'name' => ['required', 'string', 'max:255', 'unique:features,name'],
             'slug' => ['nullable', 'string', 'max:255', 'unique:features,slug'],
             'description' => ['nullable', 'string'],
